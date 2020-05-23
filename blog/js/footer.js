27ae617d3b90a1593d04ecbaaae437ffb01501b0
Matheus Vanzan <vanzan2015@gmail.com>
2020-05-23 15:41:54 +0000

    inital files

---
+++ blog/js/footer.js
@@ -0,0 +1,32 @@
+// Image full screen
+$('img.kg-image').on('click', function() {
+  $('#overlay')
+    .css({backgroundImage: `url(${this.src})`})
+    .addClass('open')
+    .one('click', function() { $(this).removeClass('open'); });
+});
+
+// Creates Captions from Alt tags
+$(".post-content img").each(function() {
+    if (!$(this).parent().is("figure")) {
+        if ($(this).attr("alt")) {
+            $(this)
+                .wrap('<figure class="image"></figure>')
+                .after( '<figcaption>' + $(this).attr("alt") + '</figcaption>');
+        }
+    }
+});
+
+// disqus coments
+var disqus = 'umcodigo';
+$("#show-disqus").click();
+
+// Send IFTTT notification    
+var url = 'https://maker.ifttt.com/trigger/acesso_um_codigo/with/key/bydgPA8SXGxqraluZW3UyS?value1=' + window.location.pathname;
+var xmlHttp = new XMLHttpRequest();
+xmlHttp.onreadystatechange = function() { 
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        console.log(xmlHttp.responseText);
+}
+xmlHttp.open('GET', url, true); // true for asynchronous 
+xmlHttp.send(null);
---
+++ blog/js/head.js
@@ -0,0 +1,9 @@
+// google analitics
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'UA-76556060-2');
+
+// ghosthunter search box
+var ghosthunter_key = 'c057032c3876780c063f9fb893';
+var ghost_root_url = '/ghost/api/v2';
